52035908cd407309b2b4c27f5353b801476821bc
Mahad <maqbom3@mcmaster.ca>
2025-01-04 19:26:53 +0300

    Completed 03.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    reversedString = "";
+    for(i = 0; i < str.length; i++) {
+        reversedString = str[i] + reversedString;
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
