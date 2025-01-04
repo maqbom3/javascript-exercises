34e97f93c909edaf21c3e30e2c6889e3bc367229
Mahad <maqbom3@mcmaster.ca>
2025-01-04 19:53:58 +0300

    Completed 05.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    sum = 0;
+    if (!Number.isInteger(num1) || !Number.isInteger(num2) || num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
 
+    if (num2 < num1) {
+        [num1, num2] = [num2, num1];
+    }
+    for(i = num1; i <= num2; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
