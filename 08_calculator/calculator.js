521c3a6ef918c91fd306d4fa9c7f2ca801c1c3ee
Mahad <maqbom3@mcmaster.ca>
2025-01-04 21:54:08 +0300

    Completed 08.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	newSum = 0;
+  for (i = 0; i < arr.length; i++) {
+    newSum += arr[i];
+  }
+  return newSum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  newProduct = 1;
+  for (i = 0; i < arr.length; i++) {
+    newProduct *= arr[i];
+  }
+  return newProduct;
 };
 
-const power = function() {
-	
+const power = function(num1,num2) {
+	return Math.pow(num1,num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num1) {
+  if (num1 === 0) {return 1;}
+	fact = 1;
+  for (i = 1; i <= num1; i++) {
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
