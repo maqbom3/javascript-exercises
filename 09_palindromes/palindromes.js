94963a3d721f3b5d658036b95962ff7e5ef0d006
Mahad <maqbom3@mcmaster.ca>
2025-01-04 22:34:51 +0300

    Completed 09 and 10.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.replace(/[ \.,?!']/g, "").toLowerCase();
+    reversedString = "";
+    for(i = 0; i < str.length; i++) {
+        reversedString = str[i] + reversedString;
+    }
+    return reversedString === str;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (typeof num === 'string') {
+        num = parseInt(num);
+    }
+    if (num < 0) {
+        return "OOPS";
+    }
+    if (num === 0 || num === 1) {
+        return num;
+    }
+    prev1 = 1;
+    prev2 = 0;
+    for(i = 0; i < num - 1; i++) {
+        temp = prev2;
+        prev2 = prev1;
+        prev1 = prev1 + temp;
+    }
+    return prev1;
+    
 };
 
 // Do not edit below this line
