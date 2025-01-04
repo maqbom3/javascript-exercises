40f745f5bba7f77917a67b4a79d5c1a2e654119b
Mahad <maqbom3@mcmaster.ca>
2025-01-04 19:21:10 +0300

    Completed 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num < 0) {
+        return `ERROR`;
+    }
+    finalString = "";
+    for(i = 0; i < num; i++) {
+        finalString = finalString + str;
+    }
+    return finalString;
 };
 
 // Do not edit below this line
